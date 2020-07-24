96f78f7f26604f014fd243382fe599dfa1c30b0e
syafzid <18052270@myrp.edu.sg>
2020-07-24 21:41:57 +0800

    Completed with Worksheet Section D

--- app/src/main/java/com/myapplicationdev/android/demoaddon/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demoaddon/MainActivity.java
@@ -3,12 +3,24 @@ package com.myapplicationdev.android.demoaddon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.iv);
+        // imageView.setImageResource(R.mipmap.ic_launcher);
+
+        // Picasso Library //
+        String imageUrl = "https://i.imgur.com/tGbaZCY.jpg";
+        Picasso.with(this).load(imageUrl).into(imageView);
     }
 }
